94398303255c3f5e6348c9d45f849dd3f3e27635
Joshua Byington <jbyington@paradowski.com>
2015-11-06 21:52:32 -0600

    tweaks to composer.json and init.php

--- init.php
+++ init.php
@@ -1,23 +1,8 @@
 <?php
 
-if( !defined(SERVER_ROOT) )
-{
-	define('SERVER_ROOT', dirname($_SERVER['DOCUMENT_ROOT']));
-}
-
-if( !defined(VENDOR_PATH) )
-{
-	define('VENDOR_PATH', SERVER_ROOT.'/vendor');
-}
-
-if( !defined(APP_PATH) )
-{
-	define('APP_PATH', SERVER_ROOT.'/app');
-}
-
-if( !defined(TEST_PATH) )
-{
-	define('TEST_PATH', APP_PATH.'/tests');
-}
+if( !defined(SERVER_ROOT) ) { define('SERVER_ROOT', dirname($_SERVER['DOCUMENT_ROOT'])); }
+if( !defined(VENDOR_PATH) ){ define('VENDOR_PATH', SERVER_ROOT.'/vendor'); }
+if( !defined(APP_PATH) ) { define('APP_PATH', SERVER_ROOT.'/app'); }
+if( !defined(TEST_PATH) ){ define('TEST_PATH', APP_PATH.'/tests'); }
 
 require_once VENDOR_PATH . '/autoload.php';
